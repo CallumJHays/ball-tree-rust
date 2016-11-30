13949a1c0b570ee4bfb7982d5f930eca2080c60a
callumjhays <callumjhays@gmail.com>
2016-11-30 06:42:56 +0000

    test

--- src/ball_tree.rs
+++ src/ball_tree.rs
@@ -31,94 +31,45 @@ pub enum BallTree<T: Baller + Clone> {
 }
 
 impl<T: Baller + Clone> BallTree<T> {
-    pub fn new() -> Self { Nil }
+    pub fn new() -> BallTree<T> { Nil }
 
-    pub fn load(collection: Vec<T>) -> Self {
-       BallTree::_load_push(collection)
-    }
-
-    fn _load_push(collection: Vec<T>) -> BallTree<T> {
-        collection.iter().fold(BallTree::new(), |bt, item| bt.push(item))
+    pub fn load(collection: &Vec<T>) -> BallTree<T> {
+       BallTree::_load_push(collection.clone())
     }
 
     pub fn flatten(&self) -> Vec<T> {
         self._flatten_node()
     }
 
-    pub fn push(&mut self, item: &T) {
-        let new_node = Point(item.clone());
-
-        match *self {
-            Nil => {},
-            _ => {
-                let (key, rad) = self._get_key_and_radius();
-                let dist = key.metric(&item);
-                if dist > rad {
-                    return self._bounding_ball(new_node);
-                }
+    pub fn push(self, features: &T) -> BallTree<T> {
+        let is_not_nil = match &self {
+            &Nil => false,
+            _ => true
+        };
+        if is_not_nil {
+            let (key, rad) = self._get_key_and_radius();
+            let dist = key.metric(&features);
+            if dist > rad {
+                return self._bounding_ball(Point(features.clone()))
             }
         }
-        
-        let mut current_self = self;
-        loop {
-            match *current_self {
-                Nil => {
-                    *current_self = new_node;
-                    break;
-                },
-                Ball(ref self_key, ref self_rad, ref left, ref right) => match new_node {
-                    Nil => {},
-                    Point(_) => {
-                        let (left_key, left_rad) = left._get_key_and_radius();
-                        let (right_key, right_rad) = right._get_key_and_radius();
-                        let left_dist = item.metric(&left_key);
-                        let right_dist = item.metric(&right_key);
-
-                        // if inside either ball, choose which ball to insert the item into
-                        if left_dist <= left_rad || right_dist <= right_rad {
-                            current_self = if left_dist < right_dist { &mut left } else { &mut right };
-                        } else {
-                            // item is in neither left nor right, wrap in new ball with the closest child
-                            if left_dist <= left_rad {
-                                left._bounding_ball(new_node);
-                            } else {
-                                right._bounding_ball(new_node);
-                            }
-                            // we have the answer. we can return now.
-                            break;
-                        }
-                    },
-                    Ball(_, _, _, _) => panic!("Adding entire balls to ball tree is not supported!")
-                },
-                Point(self_key) => {
-                    current_self._bounding_ball(new_node);
-                    break;
-                }
-            }
-        }
+        self._push_node(&Point(features.clone()))
     }
 
-    pub fn nn_search(&self, item: &T, max_entries: &usize) -> Vec<T> {
-        let mut list = self._nn_search_node(&item, &max_entries);
+    pub fn nn_search(&self, features: &T, max_entries: &usize) -> Vec<T> {
+        let mut list = self._nn_search_node(&features, &max_entries);
         list.sort_by(|a, b| {
-            a.metric(&item)
-            .partial_cmp(&b.metric(&item))
+            a.metric(&features)
+            .partial_cmp(&b.metric(&features))
             .unwrap_or(Ordering::Equal)
         });
         list
     }
 
-<<<<<<< HEAD
-    fn _load_push(collection: Vec<T>) -> Self {
-        let mut bt = BallTree::new();
-        for item in collection.into_iter() {
-            bt.push(&item);
-        }
-        bt
+    fn _load_push(collection: Vec<T>) -> BallTree<T> {
+        collection.iter().fold(BallTree::new(), |bt, item| bt.push(item))
     }
 
-=======
->>>>>>> parent of 98a76ef... made some changes
     fn _flatten_node(&self) -> Vec<T> {
         match *self {
             Nil => Vec::new(),
@@ -131,35 +82,35 @@ impl<T: Baller + Clone> BallTree<T> {
         }
     }
     
-    fn _nn_search_node(&self, item: &T, max_entries: &usize) -> Vec<T> {
+    fn _nn_search_node(&self, features: &T, max_entries: &usize) -> Vec<T> {
         match *self {
             Point(ref center) => vec![center.clone()],
             Ball(_, _, ref left, ref right) => {
                 let (left_key, left_rad) = left._get_key_and_radius();
                 let (right_key, right_rad) = right._get_key_and_radius();
-                let left_dist = item.metric(&left_key);
-                let right_dist = item.metric(&right_key);
+                let left_dist = features.metric(&left_key);
+                let right_dist = features.metric(&right_key);
 
                 let mut candidates: Vec<T> =
                     if left_dist <= left_rad || right_dist <= right_rad { // if inside either ball
                         if left_dist <= left_rad {
-                            left._nn_search_node(&item, &max_entries)
+                            left._nn_search_node(&features, &max_entries)
                         } else {
-                            right._nn_search_node(&item, &max_entries)
+                            right._nn_search_node(&features, &max_entries)
                         }
                     } else { // choose the closest one
                         if left_dist < right_dist {
-                            left._nn_search_node(&item, &max_entries)
+                            left._nn_search_node(&features, &max_entries)
                         } else {
-                            right._nn_search_node(&item, &max_entries)
+                            right._nn_search_node(&features, &max_entries)
                         }
                     };
 
                 if candidates.len() < *max_entries {
                     if left_dist < right_dist {
-                        candidates.append(&mut right._nn_search_node(&item, &max_entries));
+                        candidates.append(&mut right._nn_search_node(&features, &max_entries));
                     } else {
-                        candidates.append(&mut left._nn_search_node(&item, &max_entries));
+                        candidates.append(&mut left._nn_search_node(&features, &max_entries));
                     }
                 }
 
@@ -169,14 +120,51 @@ impl<T: Baller + Clone> BallTree<T> {
         }
     }
 
-    pub fn _bounding_ball(&mut self, other: Self) {
+    fn _push_node(self, node: &BallTree<T>) -> BallTree<T> {
+        match self {
+            Nil => node.clone(),
+            Ball(self_key, self_rad, left, right) => match *node {
+                Nil => Nil,
+                Point(ref node_key) => {
+                    let (new_left, new_right) = {
+                        let (left_key, left_rad) = left._get_key_and_radius();
+                        let (right_key, right_rad) = right._get_key_and_radius();
+                        let left_dist = node_key.metric(&left_key);
+                        let right_dist = node_key.metric(&right_key);
+
+                        // if inside either ball, choose which ball to insert the node into
+                        if left_dist <= left_rad || right_dist <= right_rad {
+                            if left_dist <= left_rad {
+                                (Box::new(left._push_node(node)), right)
+                            } else {
+                                (left, Box::new(right._push_node(node)))
+                            }
+                        } else {
+                            // node is in neither left nor right, wrap in new ball with the closest child
+                            if left_dist < right_dist {
+                                (Box::new(left._bounding_ball(node.clone())), right)
+                            } else {
+                                (left, Box::new(right._bounding_ball(node.clone())))
+                            }
+                        }
+                    };
+
+                    Ball(self_key, self_rad, new_left, new_right)
+                },
+                Ball(_, _, _, _) => panic!("Adding entire balls to ball tree is not supported!")
+            },
+            Point(self_key) => Point(self_key)._bounding_ball(node.clone())
+        }
+    }
+
+    pub fn _bounding_ball(self, other: BallTree<T>) -> BallTree<T> {
         let (self_key, self_rad) = self._get_key_and_radius();
         let (other_key, other_rad) = other._get_key_and_radius();
         let midpoint = self_key.midpoint(&self_rad, &other_key, &other_rad);
-        *self = Ball(
+        Ball(
             midpoint.clone(),
             self_key.metric(&midpoint) + self_rad,
-            Box::new(*self),
+            Box::new(self),
             Box::new(other))
     }
 
